755e5f6835bba499af4ed61e71c957afc581e89e
Maikon <Maiko552@github.com>
2024-10-29 10:33:31 -0300

    Arquivos em Java Design Patterns

--- src/main/java/ada/builder/Main.java
+++ src/main/java/ada/builder/Main.java
@@ -5,8 +5,7 @@ import ada.builder.Exemplo.PessoaComBuilder;
 
 import java.time.LocalDate;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
         //instancia sem Builder
